5d97721469e4674c7bf85a495881e3da90d6acd9
doongjohn <doongjohn@gmail.com>
2023-04-24 12:56:22 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
-// - if char is number:
-//   - state.num = number
-// - if char is operator:
-//   - if state.op_prec == current op_prec:
-//     ```
-//     state.op_list[prec].op(state.op_list[prec].num, state.num)
-//     state.op_list[prec] = None
-//     ```
-//   - if state.op_prec > current op_prec:
-//     ```
-//     for p in (0..=state.op_list.high).rev():
-//       state.op_list[p].op(state.op_list[p].num, state.num)
-//       state.op_list[p] = None
-//     ```
-//   - store operator to state.op_list[prec].op
-//   - store state.num to state.op_list[prec].num
-//   - store current op_prec to state.op_prec
+/*
+simple math expression parser
 
+- no heap allocation except `stdin().read_line()` and `format!()`
+- f32 number
+- predefined constatns
+    - 'pi', 'tau', 'e'
+- unary opeartor
+    - '+', '-'
+- infix operator
+    - '+', '-', '*', '/', '^'
+    - operator precedence
+- nested parentheses
+- simple error message
+*/
+
+/*
+- if char is number:
+    - state.num = number
+- if char is operator:
+    - if state.op_prec == current op_prec:
+      ```
+      state.op_list[prec].op(state.op_list[prec].num, state.num)
+      state.op_list[prec] = None
+      ```
+    - if state.op_prec > current op_prec:
+      ```
+      for p in (0..=state.op_list.high).rev():
+          state.op_list[p].op(state.op_list[p].num, state.num)
+          state.op_list[p] = None
+      ```
+    - store operator to state.op_list[prec].op
+    - store state.num to state.op_list[prec].num
+    - store current op_prec to state.op_prec
+*/
+
+// TODO: support builtin function (sqrt, abs, floor, ceil, round, ...)
 // TODO: support imaginary number
 // TODO: support custom variable (declaration, assignment)
-// TODO: support builtin function (sqrt, abs, floor, ceil, round, ...)
 // TODO: support custom function (infix, unary)
 
 use std::{
